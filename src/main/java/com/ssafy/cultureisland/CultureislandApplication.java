b9c4f7074325c6a7d768d56ee12ca485439acea2
서지완 <wannaseoji@naver.com>
2024-05-16 09:20:01 +0900

    feat: initial setting for project
    Signed-off-by: 이종민 <rjsqktod1@naver.com>

---
+++ src/main/java/com/ssafy/cultureisland/CultureislandApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cultureisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureislandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureislandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/cultureisland/CultureislandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.cultureisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureislandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
